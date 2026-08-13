8ad853d0f05fb4e1c57316b19eab8eb84c8ef183
sahil-mondal07 <mondalsahil591@gmail.com>
2026-08-13 19:58:15 +0530

    q1

---
+++ q1.py
@@ -0,0 +1,14 @@
+#Write a program to take two integers as input and print their sum, difference, product, quotient, and modulus.
+
+num1=int(input("enter num1 : "))
+num2=int(input("enter num2: "))
+sum=num1+num2
+diff=num1-num2
+prod=num1*num2
+quo=num1/num2
+mod=num1%num2
+print("sum : "sum)
+print("difference : ",diff)
+print("product : ",prod)
+print("quotient : ",quo)
+print("modulus : ",mod)
