6d64b54239cd51220bd3abb121e511ed530db746
Sahil Mondal <mondalsahil591@gmail.com>
2026-08-13 20:51:44 +0530

    Fix leap year condition in q4.py

--- q4.py
+++ q4.py
@@ -1,6 +1,6 @@
 #Take a year as input and check if it's a leap year.
 year=int(input("enter a year : "))
-if(year%4==0 and year%100==0 or year%400==0):
+if(((year%4==0 and year%100!=0) or  year%400==0)):
     print("its a leap year : ", year)
 else:
     print("not a leap year")
