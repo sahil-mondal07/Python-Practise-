19a70a236be0290b9afb34a5035c29c830eaf512
sahil-mondal07 <mondalsahil591@gmail.com>
2026-08-14 21:34:04 +0530

    q5

--- q5.py
+++ q5.py
@@ -4,7 +4,7 @@
     ****
     ***** """
     
-for i in range(1,6):
-    for j in range(i):
+for i in range(1,6): #range me kitne rows hai wo dhek ke range ke anar value dalni hai 
+    for j in range(i):#this loop sirf print ke liye hai 
         print("*",end="")
-    print()
+    print() 
